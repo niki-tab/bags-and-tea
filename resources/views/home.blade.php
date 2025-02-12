e5933b87d83844b552278e61b7bf309b372aed94
Nico <nicopolidis_12@hotmail.com>
2025-02-12 20:01:03 +0100

    config-files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @metadata
+<meta name="robots" content="noindex, nofollow" />
 @section('content')
 <!-- Hero Section -->
 <section class="relative overflow-hidden md:px-16">
@@ -20,30 +21,4 @@
     </div>
 </section>
 
-<!-- Featured Products Section -->
-<section class="py-16">
-    <div class="container mx-auto px-6 text-center">
-        <h2 class="text-3xl font-bold mb-8">{{ trans('pages/home.featured_products') }}</h2>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            @foreach($featuredProducts as $product)
-            <div class="bg-white rounded-lg shadow-lg overflow-hidden">
-                <img src="{{ asset($product->image) }}" alt="{{ $product->name }}" class="w-full h-48 object-cover">
-                <div class="p-6">
-                    <h3 class="text-xl font-semibold mb-2">{{ $product->name }}</h3>
-                    <div class="text-left">
-                    <p class="text-l text-black mb-2"><strong>{{ trans('pages/home.label-product-origin') }}  </strong>{{ $product->origin_general }}</p>
-                    <span class="text-l text-black mb-4"><strong>{{ trans('pages/home.label-product-price-from') }} </strong>{{ $product->price }} €</span>
-                    </div>
-                    @if($product->out_of_stock == false)
-                        <p class="text-l text-white my-6 bg-green-600 rounded px-2 inline-block">{{ trans('pages/home.label-product-stock-available') }}</p>
-                    @else
-                        <p class="text-l text-white my-6 bg-red-700 rounded px-2 inline-block">{{ trans('pages/home.label-product-no-stock-available') }}</p>
-                    @endif
-                    <a href="{{ route(app()->getLocale() === 'es' ? 'product.show.es' : 'product.show.en', ['locale' => app()->getLocale(), 'productSlug' => $product->slug]) }}" class="block mt-4 bg-button-color-1 text-white py-2 px-4 rounded-lg text-center hover:bg-sky-800 hover:text-button-color-1-hover">{{ trans('pages/home.view_product') }}</a>
-                </div>
-            </div>
-            @endforeach
-        </div>
-    </div>
-</section>
 @endsection
