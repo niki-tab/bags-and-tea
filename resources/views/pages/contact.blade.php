0ebc015e76de8a33a2b3759a8db0decbcd532763
Nico <nicopolidis_12@hotmail.com>
2025-02-12 20:02:44 +0100

    config-files

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+<meta name="robots" content="noindex, nofollow" />
 @section('content')
 <div class="container mx-auto p-4">
     <h1 class="text-3xl font-bold text-left mb-6 ml-4 mt-8 md:ml-16 md:mt-4">{{ trans('pages/contact.title-page') }}</h1>
--- resources/views/pages/product-detail.blade.php
+++ resources/views/pages/product-detail.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+<meta name="robots" content="noindex, nofollow" />
 @section('content')
         @livewire('product-detail', ['productSlug' => request()->route('productSlug')])
         @livewire('product-information-tab')
