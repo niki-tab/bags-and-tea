7b7da3e335420bca943a01a7cb325c5c9ebbe96c
Nico <nicopolidis_12@hotmail.com>
2025-03-04 20:51:26 +0100

    prueba

--- app/Http/Middleware/RedirectIfNoLocale.php
+++ app/Http/Middleware/RedirectIfNoLocale.php
@@ -19,13 +19,13 @@ class RedirectIfNoLocale
             return $next($request); // Skip the language prefix for Livewire routes
         }
 
-        $locale = $request->segment(1); 
+        /*$locale = $request->segment(1); 
 
         // Check if the first segment is a valid locale ('en' or 'es')
         if (!in_array($locale, ['en', 'es'])) {
             // Redirect to the default locale (e.g., 'es') if no valid locale is found
             return redirect('/es' . $request->getRequestUri());
-        }
+        }*/
 
         return $next($request);
     }
