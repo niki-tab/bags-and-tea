b84785c22aa3c437244006626591dec42acaa245
Nico <nicopolidis_12@hotmail.com>
2025-04-01 09:20:08 +0200

    vestiaire

--- resources/views/components/header-mobile.blade.php
+++ resources/views/components/header-mobile.blade.php
@@ -18,32 +18,52 @@
             </a>
         </div>
         <div class="flex items-center gap-3 mt-5 mr-6">
-            <a href="{{ route(app()->getLocale() === 'es' ? 'login.show.en-es' : 'login.show.en-es', ['locale' => app()->getLocale()]) }}">
-                <img src="{{ request()->routeIs('login.show.en-es') ? asset('images/icons/mobile_search_icon_clicked.svg') : asset('images/icons/mobile_search_icon.svg') }}" 
-                    class="w-6 h-6 cursor-pointer"
-                    onmouseover="this.src='{{ request()->routeIs('login.show.en-es') ? asset('images/icons/mobile_search_icon_clicked.svg') : asset('images/icons/mobile_search_icon_clicked.svg') }}'"
-                    onmouseout="this.src='{{ request()->routeIs('login.show.en-es') ? asset('images/icons/mobile_search_icon_clicked.svg') : asset('images/icons/mobile_search_icon.svg') }}'">
-            </a>
-            <a class="mb-1" href="{{ route(app()->getLocale() === 'es' ? 'cart.edit.es' : 'cart.edit.en', ['locale' => app()->getLocale()]) }}">
-                <img src="{{ request()->routeIs('cart.edit.es') || request()->routeIs('cart.edit.en') ? asset('images/icons/mobile_cart_icon_clicked.svg') : asset('images/icons/mobile_cart_icon.svg') }}" 
-                    class="w-8 h-8 cursor-pointer"
-                    onmouseover="this.src='{{ request()->routeIs('cart.edit.es') || request()->routeIs('cart.edit.en') ? asset('images/icons/mobile_cart_icon_clicked.svg') : asset('images/icons/mobile_cart_icon_clicked.svg') }}'"
-                    onmouseout="this.src='{{ request()->routeIs('cart.edit.es') || request()->routeIs('cart.edit.en') ? asset('images/icons/mobile_cart_icon_clicked.svg') : asset('images/icons/mobile_cart_icon.svg') }}'">
-            </a>
             <button id="hamburgerMenu" class="focus:outline-none">
-                <img src="{{ asset('images/icons/mobile_burguer_menu_icon.svg') }}" alt="hamburger menu" width="25"
-                onmouseover="this.src='{{ asset('images/icons/mobile_burguer_menu_icon_clicked.svg') }}'"
-                onmouseout="this.src='{{ asset('images/icons/mobile_burguer_menu_icon.svg') }}'">
+                <img src="{{ asset('images/icons/mobile_burguer_menu_icon.svg') }}" alt="hamburger menu" width="25" id="menuIcon"
+                data-menu-open="{{ asset('images/icons/icon_hamburguer_menu_close.svg') }}"
+                data-menu-closed="{{ asset('images/icons/mobile_burguer_menu_icon.svg') }}">
             </button>
         </div>
     </div>
         
     <div id="hamburgerMenuOptions" class="hidden bg-background-color-4 w-full py-4 mt-4">
         <div class="relative w-4/5 mx-auto mb-4">
-            <img src="{{ asset('images/icons/vector_search_icon.svg') }}" 
+            <img src="{{ asset('images/icons/vector_search_icon.svg') }}"  
                     class="absolute left-5 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" 
                     alt="Search Icon">
             <input type="text" class="pl-12 placeholder:font-robotoCondensed placeholder:text-color-2  placeholder:font-light w-full p-2 bg-white text-color-2 rounded-full border border-gray-300 font-robotoCondensed" placeholder="{{ trans('components/header.placeholder-input-search-product') }}">
+        </div>
+        <div class="flex items-center gap-3 justify-center">
+            <a target="_blank" href="https://es.vestiairecollective.com/profile/30176798/?sortBy=relevance&tab=items-for-sale">
+                <img src="{{asset('images/icons/mobile_header_instagram.svg') }}" 
+                    class="w-6 h-6 cursor-pointer"
+                    onmouseover="this.src='{{asset('images/icons/mobile_header_instagram_hover.svg') }}'"
+                    onmouseout="this.src='{{asset('images/icons/mobile_header_instagram.svg') }}'">
+            </a>
+            <a target="_blank" href="https://www.vinted.es/member/250362636-bagsandtea">
+                <img src="{{asset('images/icons/mobile_header_vinted.svg') }}" 
+                    class="w-6 h-6 cursor-pointer"
+                    onmouseover="this.src='{{asset('images/icons/mobile_header_vinted_hover.svg') }}'"
+                    onmouseout="this.src='{{asset('images/icons/mobile_header_vinted.svg') }}'">
+            </a>
+            <a target="_blank" href="https://es.vestiairecollective.com/profile/30176798/?sortBy=relevance&tab=items-for-sale">
+                <img src="{{asset('images/icons/mobile_header_vestaire.svg') }}" 
+                    class="w-6 h-6 cursor-pointer"
+                    onmouseover="this.src='{{asset('images/icons/mobile_header_vestaire_hover.svg') }}'"
+                    onmouseout="this.src='{{asset('images/icons/mobile_header_vestaire.svg') }}'">
+            </a>
+            <a href="{{ route(app()->getLocale() === 'es' ? 'login.show.en-es' : 'login.show.en-es', ['locale' => app()->getLocale()]) }}" class="ml-6">
+                <img src="{{ request()->routeIs('login.show.en-es') ? asset('images/icons/mobile_user_avatar_clicked.svg') : asset('images/icons/mobile_user_avatar.svg') }}" 
+                    class="w-6 h-6 cursor-pointer"
+                    onmouseover="this.src='{{ request()->routeIs('login.show.en-es') ? asset('images/icons/mobile_user_avatar_clicked.svg') : asset('images/icons/mobile_user_avatar_hover.svg') }}'"
+                    onmouseout="this.src='{{ request()->routeIs('login.show.en-es') ? asset('images/icons/mobile_user_avatar_clicked.svg') : asset('images/icons/mobile_user_avatar.svg') }}'">
+            </a>
+            <a class="mb-1" href="{{ route(app()->getLocale() === 'es' ? 'cart.edit.es' : 'cart.edit.en', ['locale' => app()->getLocale()]) }}">
+                <img src="{{ request()->routeIs('cart.edit.es') || request()->routeIs('cart.edit.en') ? asset('images/icons/mobile_cart_icon_clicked_2.svg') : asset('images/icons/mobile_cart_icon.svg') }}" 
+                    class="w-8 h-8 cursor-pointer"
+                    onmouseover="this.src='{{ request()->routeIs('cart.edit.es') || request()->routeIs('cart.edit.en') ? asset('images/icons/mobile_cart_icon_clicked_2.svg') : asset('images/icons/mobile_cart_icon_clicked.svg') }}'"
+                    onmouseout="this.src='{{ request()->routeIs('cart.edit.es') || request()->routeIs('cart.edit.en') ? asset('images/icons/mobile_cart_icon_clicked_2.svg') : asset('images/icons/mobile_cart_icon.svg') }}'">
+            </a>
         </div>
         <div class="space-y-4 mt-8">
             <a href="{{ route(app()->getLocale() === 'es' ? 'certify-your-bag.show.es' : 'certify-your-bag.show.en', ['locale' => app()->getLocale()]) }}" class="{{ request()->routeIs('certify-your-bag.show.es') || request()->routeIs('certify-your-bag.show.en') ? 'font-bold text-theme-color-2' : 'text-color-2' }} block text-lg hover:underline pb-2 font-robotoCondensed ml-12">{{ trans('components/header.menu-option-1') }}</a>
@@ -58,18 +78,17 @@
 <script>
     // JavaScript to toggle the menu
     document.getElementById('hamburgerMenu').addEventListener('click', function() {
-        var menu = document.getElementById('mobileMenu');
-        if (menu.classList.contains('hidden')) {
-            menu.classList.remove('hidden');
-        } else {
-            menu.classList.add('hidden');
-        }
-    });
-        
-    
-    document.getElementById('hamburgerMenu').addEventListener('click', function() {
         const menu = document.getElementById('hamburgerMenuOptions');
+        const icon = document.getElementById('menuIcon');
+        
         menu.classList.toggle('hidden');
+        
+        // Toggle icon
+        if (menu.classList.contains('hidden')) {
+            icon.src = icon.dataset.menuClosed;
+        } else {
+            icon.src = icon.dataset.menuOpen;
+        }
     });
 
 </script>
