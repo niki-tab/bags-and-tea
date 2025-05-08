7afbaab6a480ff7bbfa16616f94a2ad6cc31ab94
Nico <nicopolidis_12@hotmail.com>
2025-05-08 19:52:34 +0200

    fav icon

--- app/Http/Controllers/WeBuyYourBagController.php
+++ app/Http/Controllers/WeBuyYourBagController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Lang;
+
 
 
 class WeBuyYourBagController extends Controller
@@ -19,9 +21,34 @@ class WeBuyYourBagController extends Controller
 
     public function setSeo(){
 
-        seo()
-        ->title(trans('pages/we-buy-your-bag.page-seo-title'), env('APP_NAME'))
-        ->description(trans('pages/we-buy-your-bag.page-seo-description'));
+        if(request()->route('bagName')){
+
+            $bagName = request()->route('bagName');
+    
+            $translationKey = 'pages/we-buy-your-bag.page-seo-title-' . $bagName;
+            $seoMetaTitle = Lang::has(key: $translationKey) 
+                ? trans($translationKey) 
+                : trans('pages/we-buy-your-bag.page-seo-title');    
+            
+            $translationKey = 'pages/we-buy-your-bag.page-seo-description-' . $bagName;
+            $seoMetaDescription = Lang::has(key: $translationKey) 
+                ? trans($translationKey) 
+                : trans('pages/we-buy-your-bag.page-seo-description');    
+
+                seo()
+            ->title($seoMetaTitle, env('APP_NAME'))
+            ->description($seoMetaDescription);
+
+        }else{
+
+            seo()
+            ->title(trans('pages/we-buy-your-bag.page-seo-title'), env('APP_NAME'))
+            ->description(trans('pages/we-buy-your-bag.page-seo-description'));
+            
+        }
+
+        
+        
         
     }
 
