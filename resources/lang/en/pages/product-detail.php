fb15d303f69062c9a8980a98fe520e49ca4230a4
Nicolás Tabares Wiede <nicolas.tabares.tech@gmail.com>
2025-07-17 23:02:38 +0100

    yias

--- resources/lang/en/pages/product-detail.php
+++ resources/lang/en/pages/product-detail.php
@@ -5,5 +5,6 @@ return [
     'label-quantity' => 'Quantity',
     'label-select-product-size' => 'Size',
     'button-add-to-cart' => 'Add to cart',
-    
+    'certificate-title' => 'Authenticity Certificate',
+    'certificate-text' => 'This product has been authenticated and is covered by our lifetime warranty.',
 ];
