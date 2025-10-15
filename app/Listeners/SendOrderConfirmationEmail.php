57ca531e2002addb448d85b9cca6f2c9fe616564
Nicolás Tabares Wiede <nicolas.tabares.tech@gmail.com>
2025-10-15 18:47:45 +0100

    order confirmation bcc

--- app/Listeners/SendOrderConfirmationEmail.php
+++ app/Listeners/SendOrderConfirmationEmail.php
@@ -47,7 +47,7 @@ class SendOrderConfirmationEmail implements ShouldQueue
             
             // Send the order confirmation email with BCC (including Trustpilot for review invitations)
             Mail::to($order->customer_email)
-                ->bcc(['nicolas.tabares.tech@gmail.com', 'bagsandtea.com+d98660a3fa@invite.trustpilot.com'])
+                ->bcc(['nicolas.tabares.tech@gmail.com', 'info@sweetba.com'])
                 ->send(new OrderConfirmation($event->orderNumber));
             
             Log::info('Order confirmation email sent successfully for order: ' . $event->orderNumber);
