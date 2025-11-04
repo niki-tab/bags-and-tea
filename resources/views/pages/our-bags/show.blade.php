0935fa324421ffefb6b0c6779d7fb8581d6d9e5c
Manu-villar <manuel.villarna@gmail.com>
2025-11-04 12:00:39 +0000

    added our-bags view

--- resources/views/pages/our-bags/show.blade.php
+++ resources/views/pages/our-bags/show.blade.php
@@ -16,60 +16,62 @@ bg-background-color-4
 
 @section('content')
 
-<div class="bg-[#F6F0ED] py-12 px-4 md:px-16">
-    <div class="max-w-7xl mx-auto">
-        <!-- Título Principal -->
+<div class="bg-[#F6F0ED] py-12 px-2 md:px-4 lg:px-6">
+
+    <div class="max-w-6xl mx-auto">
+
+        
         <div class="text-center mb-12">
             <h1 class="mt-[20px] mb-[40px] text-4xl md:text-5xl font-rosaline text-[#482626]">
                 {{ trans('pages/our-bags.main_title') }}
             </h1>
 
-            <p class="text-left text-lg font-robotoCondensed text-[#482626] max-w-4xl mx-auto mb-8">
+            <p class="text-left text-lg font-robotoCondensed text-[#482626] mb-8">
                 {{ trans('pages/our-bags.main_description') }}
             </p>
 
-            <p class="text-left text-lg font-robotoCondensed text-[#482626] max-w-4xl mx-auto mb-8">
+            <p class="text-left text-lg font-robotoCondensed text-[#482626] mb-8">
                 {{ trans('pages/our-bags.main_description_1') }}
             </p>
 
             <img
                 src="{{ asset('images/our-bags/MaskGroup.png') }}"
                 alt="Bags & Tea"
-                class="mx-auto max-w-full h-auto"
+                class="mx-auto w-full h-auto"
             >
         </div>
 
         <div class="text-center mb-12">
-            <h2 class="text-left max-w-4xl mx-auto mt-[70px] mb-[30px] text-3xl md:text-4xl font-rosaline text-[#AC2231]">
+            <h2 class="text-left mt-[70px] mb-[30px] text-3xl md:text-4xl font-rosaline text-[#AC2231]">
                 {{ trans('pages/our-bags.main_title_2') }}
             </h2>
-            <p class="text-left text-lg font-robotoCondensed text-[#482626] max-w-4xl mx-auto mb-8">
+
+            <p class="text-left text-lg font-robotoCondensed text-[#482626] mb-8">
                 {!! trans('pages/our-bags.main_description_2') !!}
             </p>
-            <p class="text-left text-lg font-robotoCondensed text-[#482626] max-w-4xl mx-auto mb-8">
+            <p class="text-left text-lg font-robotoCondensed text-[#482626] mb-8">
                 {!! trans('pages/our-bags.main_description_3') !!}
             </p>
-            <p class="text-left text-lg font-robotoCondensed text-[#482626] max-w-4xl mx-auto mb-8">
+            <p class="text-left text-lg font-robotoCondensed text-[#482626] mb-8">
                 {!! trans('pages/our-bags.main_description_4') !!}
             </p>
-            <div class="flex flex-col md:flex-row gap-4 justify-center items-center">
+
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 justify-center items-start">
                 <img
                     src="{{ asset('images/our-bags/MaskGroup_1.png') }}"
                     alt="Bags & Tea"
-                    class="max-w-full h-auto"
+                    class="w-full h-auto"
                 >
                 <img
                     src="{{ asset('images/our-bags/MaskGroup_2.png') }}"
                     alt="Bags & Tea"
-                    class="max-w-full h-auto"
+                    class="w-full h-auto"
                 >
             </div>
         </div>
-        
-    </div>
-</div>
+    </div> 
 
-<!-- Banner adicional -->
+</div>
 
 @endsection
 
