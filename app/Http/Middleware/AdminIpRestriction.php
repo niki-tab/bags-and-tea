410c7ac5c6de8bcbbad25cbbc0be2ccb9519b2aa
Nicolás Tabares Wiede <nicolas.tabares.tech@gmail.com>
2026-01-11 17:39:07 +0000

    wep

--- app/Http/Middleware/AdminIpRestriction.php
+++ app/Http/Middleware/AdminIpRestriction.php
@@ -14,7 +14,7 @@ class AdminIpRestriction
      * Add your static IPs here. Consider using environment variables for flexibility.
      */
     protected array $allowedIps = [
-        '88.8.191.153',
+        '88.8.192.109',
         '79.116.237.154',
         
     ];
